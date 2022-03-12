ecf23bbd291ebbcf66224cb0ae508bfdb412da06
oguzn.sari <oguzn.sari@gmail.com>
2022-03-12 19:12:09 +0300

    2nd step - GET request for /poll endpoint

--- server/index.js
+++ server/index.js
@@ -1,3 +1,25 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path ");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+const dataFile = path.join(__dirname, "data.json");
+
+// Support POSTing form data with URL encoded
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/poll", async (req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+    const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
+
+    data = Object.entries(data).map(([label, votes]) => {
+        return {
+            label: label,
+            percentage: ((100 * votes) / totalVotes) || 0
+        }
+    });
+
+    res.json(data);
+});
+
+app.listen(3000, () => console.log("Server is running..."));
