94eb3e2d06f94f4b77da81e1fda9970d4ae150e1
oguzn.sari <oguzn.sari@gmail.com>
2022-03-12 19:32:08 +0300

    3rd step - POST request for /poll endpoint

--- server/index.js
+++ server/index.js
@@ -22,4 +22,13 @@ app.get("/poll", async (req, res) => {
     res.json(data);
 });
 
+app.post("/poll", async (req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+    
+    data[req.body.add]++;
+
+    await fs.writeFile(dataFile, JSON.stringify(data));
+    res.end();
+});
+
 app.listen(3000, () => console.log("Server is running..."));
